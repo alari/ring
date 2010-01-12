f8c0fdc46233ad9c758833fee9b941654abd0a5e
alari <name.alari@gmail.com>
2010-01-12 17:21:59 +0000

    phar experiment

--- entry.php
+++ entry.php
@@ -1,5 +1,6 @@
 <?php
 Header("Content-type: text/html; charset=utf-8");
 const O_DOC_ROOT = __DIR__;
-require './O/src/EntryPoint.phps';
+require 'phar://'.O_DOC_ROOT.'/O/o.src.phar';
+//require './O/src/EntryPoint.phps';
 O_EntryPoint::processRequest();
