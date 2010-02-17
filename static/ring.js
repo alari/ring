80a1deeeb08ea8b2b3ca3f648ce983080d66158f
alari <name.alari@gmail.com>
2010-02-17 00:16:39 +0000

    reverted ring

--- static/ring.js
+++ static/ring.js
@@ -7,18 +7,18 @@ var R = {
 		},
 		show: function(){
 			R.Shadow.show();
-			this.getEl().fadeIn();
+			this.getEl().fade('show');
 			this.isShowed = 1;
 		},
 		hide: function(){
 			R.Shadow.hide();
-			this.getEl().fadeOut();
+			this.getEl().fade('hide');
 			this.isShowed = 0;
 		},
 		getEl: function(){
 			if(!this.el) {
-				this.el = $('#user-menu');
-				this.el.css('width', $('#user-box').width());
+				this.el = $('user-menu');
+				this.el.style.width = $('user-box').getSize().x;
 			}
 			return this.el;
 		}
@@ -27,75 +27,67 @@ var R = {
 		el: null,
 		isShowed:0,
 		show: function(){
-			this.el.css('width', $('#wrap').width());
-			this.el.css('height', $('#wrap').height());
-			this.getEl().fadeTo(400, 0.6);
+			this.getEl().style.width = $('wrap').getParent().getScrollSize().x;
+			this.getEl().style.height = $('wrap').getParent().getScrollSize().y;
+			this.getEl().fade(0.6);
 			this.isShowed = 1;
 		},
 		hide: function(){
-			this.getEl().fadeTo("fast", 0, function(){
-				$(this).fadeOut();
-			});
+			this.getEl().fade("out");
 			this.isShowed = 0;
 		},
 		getEl: function(){
 			if(!this.el) {
 				this.el = new Element("div", {id:"shadow"});
-				this.el.fadeOut();
-				$('#wrap').after(this.el);
+				this.el.fade('hide');
+				$('wrap').getParent().adopt(this.el);
 				this.el.style.left = 0;
 				this.el.style.top = 0;
 			}
 			return this.el;
 		},
-		toggle: function(){
-			this.isShowed ? this.hide() : this.show();
+		toggle: function(widthOffset){
+			this.isShowed ? this.hide() : this.show(widthOffset);
 		}
 	},
 	AjaxFragment: {
-		show: function(elName, options) {
-			el = $('#'+elName);
+		show: function(el, options) {
+			el = $(el);
 			this.init(el);
-			el.attr("r_ajax_isShowed", 1);
-			el.css("overflow", "hidden");
-			
-			el.load(options.url, options.data, function() {
-				$(this).animate({				 
-				    height: $(this).innerHeight()
-				  }, 850, function() {
-					  $(this).css('height', 'auto');
-					  $(this).css('overflow', 'visible');
-				  });
-			});
+			el.set("r_ajax_isShowed", 1);
+			el.style.overflow = "hidden";
+			new Request($merge(options,{onSuccess:function(response){
+				el.set('html', response);
+				el.get('tween', {property:'height'}).start(0,el.getScrollSize().y).chain(function(){el.style.height='auto';el.style.overflow='visible';});
+			}})).send();
 		},
 		hide: function(el) {
-			el = $('#'+el);
-			el.css('overflow', 'hidden');
-			el.animate({height: $(this).innerHeight()}, 850);
-			el.attr("r_ajax_isShowed", 0);
+			el = $(el);
+			el.style.overflow='hidden';
+			el.get('tween',{property:'height'}).start(el.getScrollSize().y, 0);
+			el.set("r_ajax_isShowed", 0);
 		},
 		init: function(el, param) {
-			if(el.attr('r_ajax_isInitiated') == 1) return;
-			el.css('height', 0);
-			el.attr('r_ajax_isInitiated', 1);
+			if(el.get('r_ajax_isInitiated') == 1) return;
+			el.style.height = 0;
+			el.set('r_ajax_isInitiated', 1);
 		},
-		toggle: function(elName, options) {
-			el = $("#" + elName);
-			if(el.attr("r_ajax_isShowed") == 1) this.hide(elName);
-			else this.show(elName, options);
+		toggle: function(el, options) {
+			el = $(el);
+			if(el.get("r_ajax_isShowed") == 1) this.hide(el);
+			else this.show(el, options);
 		}
 	},
 	Comment: {
-		/*
-		 * .showForm(this,'<?=O_UrlBuilder::get( "comment" )?>',<?=$rootId?>,<?=$parent?>,<?=$systemId?>)
-		 */
 		showForm: function(el, url, root, parent, sys) {
-			el = $(el).parent();
-			e = el.children('.form-el');
-			if(e.lenght == 0) {
-				el.children('.form-el').after('<div class="form-el"></div>');
+			el = $(el).getParent();
+			e=el.retrieve("form-el");
+			if(!e) {
+				var e=new Element('div');
+				e.injectAfter(el);
+				el.store("form-el", e);
 			}
-			if(e.attr("r_ajax_isShowed") == 1) {
+			if(e.get("r_ajax_isShowed") == 1) {
 				return R.AjaxFragment.hide(e);
 			}
 			var action = parent?'comment-for':'comment-new';
@@ -109,89 +101,75 @@ var R = {
 		},
 		remove: function(url, root, comm, sys) {
 			if(!confirm("Удалить комментарий со всеми ответами на него?")) return;
-			$('#comm-'+comm).fadeTo(400, 0.5);
-			$('#comm-add-'+comm).fadeTo(400, 0.4);
-			$.post(url, {
+			$('comm-'+comm).fade(0.5);
+			$('comm-add-'+comm).fade(0.4);
+			new Request.JSON({url:url, data:{
 				root: root,
 				action: 'delete',
 				comm: comm,
-				sys: sys}, function(response){
+				sys: sys}, onSuccess:function(response){
 					if(response.status == "FAILED") {
 						alert("Ошибка! Удалить комментарий не удалось.");
 						return;
 					}
 					for(j in response.comments) {
 						i = response.comments[j];
-						$('#comm-'+i).dispose();
-						$('#comm-add-'+i).dispose();
+						$('comm-'+i).dispose();
+						$('comm-add-'+i).dispose();
 					}
-				},
-				"json"
-			);
-		}	
+				}}).send();
+		}
 	},
 	System: {
 		url: null,
 		setSortable: function(list, handle, host) {
 			this.url = 'http://'+host+'/admin/system-position';
-			$(list).Sortable({
-				start:R.SortableUtils.onStart,
-				update: this.onComplete,
-				handle: handle
-			});
+			new Sortables(list, {'handle':handle, onStart:R.SortableUtils.onStart, onComplete:this.onComplete.bind(this)});
 		},
-		onComplete:function(elName){
-			el = $("#"+elName);
-			var newPosition = el.prevUntil().length+1;
-			if(newPosition == R.SortableUtils.oldPosition-1) return;
+		onComplete:function(el){
+			el = $(el);
+			 var newPosition = el.getAllPrevious().length+1;
+			 if(newPosition == R.SortableUtils.oldPosition-1) return;
 		 	
 		 	var elId = el.get("id").replace(/^sysid-(.+)$/, "$1");
-		 	$.post(this.url, {base:elId, pos:newPosition});
+		 	new Request({url:this.url,data:{base:elId,pos:newPosition}}).post();
 		 }
 	},
 	Collection: {
 		url: null,
 		setSortable: function(list, handle, host) {
 			this.url = 'http://'+host+'/admin/collection-position';
-			$(list).Sortable({
-				start:R.SortableUtils.onStart,
-				update: this.onComplete,
-				handle: handle
-			});
+			new Sortables(list, {'handle':handle, onStart:R.SortableUtils.onStart, onComplete:this.onComplete.bind(this)});
 		},
 		onComplete:function(el){
-			el = $("#"+elName);
-			var newPosition = el.prevUntil().length+1;
-			if(newPosition == R.SortableUtils.oldPosition-1) return;
+			el = $(el);
+			 var newPosition = el.getAllPrevious().length+1;
+			 if(newPosition == R.SortableUtils.oldPosition-1) return;
 		 	
-			var elId = el.get("id").replace(/^collid-([0-9]+)$/, "$1");
-			$.post(this.url, {base:elId, pos:newPosition});
+		 	var elId = el.get("id").replace(/^collid-([0-9]+)$/, "$1");
+		 	new Request({url:this.url,data:{coll:elId,pos:newPosition}}).post();
 		 }
 	},
 	Anonce: {
 		url: null,
 		setSortable: function(list, handle, host) {
 			this.url = 'http://'+host+'/admin/anonce-position';
-			$(list).Sortable({
-				start:R.SortableUtils.onStart,
-				update: this.onComplete,
-				handle: handle
-			});
+			new Sortables(list, {'handle':handle, onStart:R.SortableUtils.onStart, onComplete:this.onComplete.bind(this)});
 		},
-		onComplete: function(elName) {
-			 el = $("#"+elName);
-			 var newPosition = el.prevUntil().length+1;
+		onComplete: function(el) {
+			 el = $(el);
+			 var newPosition = el.getAllPrevious().length+1;
 			 if(newPosition == R.SortableUtils.oldPosition-1) return;
 			 	
 			 var elId = el.get("id").replace(/^anonceid-(.+)$/, "$1");
-			 $.post(this.url, {base:elId, pos:newPosition});
+			 new Request({url:this.url,data:{anonce:elId,pos:newPosition}}).post();
 		}
 	},
 	SortableUtils: {
 		oldPosition: null,
-		onStart: function(elName) {
-			el = $("#"+elName);
-			R.SortableUtils.oldPosition = el.prevUntil().length+1; 
+		onStart: function(el) {
+			el = $(el);
+			R.SortableUtils.oldPosition = el.getAllPrevious().length+1; 
 		}
 	}
 };
